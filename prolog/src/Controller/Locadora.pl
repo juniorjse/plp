6f6c260611538e787bc7407b4da1b877d2b21605
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2023-10-24 19:30:09 -0300

    novo

--- prolog/src/Controller/Locadora.pl
+++ prolog/src/Controller/Locadora.pl
@@ -25,45 +25,43 @@ menuLocadora :-
     otherwise -> writeln('Opção inválida. Tente novamente.')),
     menuLocadora.
 
-    removerCarro:-
-        get_connection(Connection),
-        writeln('Informe o ID do carro que deseja remover:'),
-        read_line_to_string(user_input, IdCarroString),
-        (isANumber(IdCarro, IdCarroString) ->
-            checkCarroStatus(Connection, IdCarro, Status),
-            (Status = 'O' ->
-                writeln('Este carro encontra-se alugado. Escolha outro ou retorne ao menu principal.');
-                Status = 'D' ->
-                    confirmarRemocao(Connection, IdCarro);
-                Status = 'R' ->
-                    writeln('Este carro está em reparo. Escolha outro ou retorne ao menu principal.');
-                otherwise ->
-                    writeln('Carro não encontrado.');
-            );
-            writeln('ID informado é inválido. Tente novamente.')
-        ),
-        encerrandoDatabase(Connection).
+    listarAlugueisPorPessoa :-
+        writeln('Digite o ID do cliente para listar os registros de aluguéis:'),
+        util:get_input('', ClienteIDStr),
+        writeln(''),
     
-    confirmarRemocao(Connection, IdCarro):-
-        writeln('Deseja realmente remover este carro? (s/n):'),
-        read_line_to_string(user_input, Confirmacao),
-        (Confirmacao = "s" ->
-            removeCarro(Connection, IdCarro),
-            writeln('Carro removido com sucesso.');
-            Confirmacao = "n" ->
-                writeln('Remoção cancelada.');
-            otherwise ->
-                writeln('Opção inválida. Tente novamente.')
+        (util:isANumber(ClienteID, ClienteIDStr) ->
+            connectiondb:iniciandoDatabase(Connection),
+            (clienteExiste(Connection, ClienteID) ->
+                user_operations:getAlugueisPorPessoa(Connection, ClienteID, Alugueis),
+                (length(Alugueis, NumRegistros), NumRegistros > 0 ->
+                    writeln('Registros de Aluguéis:'),
+                    mostrarRegistrosDeAlugueis(Connection, Alugueis)
+                ;
+                    writeln('Não há registros de aluguéis para este cliente.')
+                ),
+                connectiondb:encerrandoDatabase(Connection)
+            ;
+                writeln('Cliente não encontrado na base de dados.')
+            )
+        ;
+            writeln('ID de cliente inválido. Tente novamente.')
         ).
     
-    checkCarroStatus(Connection, IdCarro, Status):-
-        getCarro(Connection, IdCarro, [_, _, _, _, _, _, Status|_]).
+    mostrarRegistrosDeAlugueis(_, []).
+    mostrarRegistrosDeAlugueis(Connection, [Registro | RegistrosRestantes]) :-
+        mostrarRegistroDeAluguel(Connection, Registro),
+        mostrarRegistrosDeAlugueis(Connection, RegistrosRestantes).
     
-    removeCarro(Connection, IdCarro):-
-        db_parameterized_query_no_return(
-            Connection, 
-            "DELETE FROM Carros WHERE id_carro = %w;",
-            [IdCarro]
-        ).
-    
-    :- initialization(menuLocadora).
+    mostrarRegistroDeAluguel(Connection, [AluguelID, CarroID | OutrasInformacoes]) :-
+        user_operations:getCarro(Connection, CarroID, CarroInfo), 
+        user_operations:verificaTempoAluguel(Connection, [AluguelID | OutrasInformacoes], DataInicio), 
+        user_operations:verificaTempoAluguel(Connection, [AluguelID | OutrasInformacoes], DataDevolucao), 
+        user_operations:verificaTempoAluguel(Connection, [AluguelID | OutrasInformacoes], Valor), 
+        user_operations:verificaTempoAluguel(Connection, [AluguelID | OutrasInformacoes], Status), 
+        writeln('Carro:               '), writeln(CarroInfo), 
+        writeln('Data de Início:      '), writeln(DataInicio), 
+        writeln('Data de Devolução:   '), writeln(DataDevolucao), 
+        writeln('Valor do Aluguel:    R$ '), writeln(Valor), 
+        writeln('Status do Aluguel:   '), writeln(Status), 
+        writeln('').
